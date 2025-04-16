13834ef7bb419dcbe483c9331ce50a73e68b57a8
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-16 15:08:54 +0530

    updated components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Signup from './components/Signup/Signup.jsx';
-import Login from './components/login/Login.jsx';
+// import Signup from './components/Signup/Signup.jsx';
+// import Login from './components/login/Login.jsx';
 import Index from './components/Home/Index.jsx';
 import{ RouterProvider, createBrowserRouter } from 'react-router-dom';
 function App() {
@@ -9,14 +9,14 @@ function App() {
       path: '/',
       element: <Index />,
     },
-    {
-      path: '/Signup',
-      element: <Signup />,
-    },
-    {
-      path: '/login',
-      element: <Login />,
-    },
+    // {
+    //   path: '/Signup',
+    //   element: <Signup />,
+    // },
+    // {
+    //   path: '/login',
+    //   element: <Login />,
+    // },
   ])
   return (
     <div className="App">
